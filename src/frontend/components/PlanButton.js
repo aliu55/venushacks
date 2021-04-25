b63193fb72de9130314452e0042dfd4b7433ea8f
Ashley Liu <aslorange61@gmail.com>
2021-04-24 23:32:52 -0700

    PlanButton

--- src/frontend/components/PlanButton.js
+++ src/frontend/components/PlanButton.js
@@ -1,18 +1,27 @@
 import React, { useState, useEffect } from 'react';
 import './PlanButton.css';
 
-const PlanButton = ({ userSelection }) => {
-    const [results, setResults] = useState({
-        "title": [],
-        "thumbnail": [],
-        "url": []
-    })
+const PlanButton = ({ userSelection, results, setResults }) => {
 
     const handleClick = () => {
-        // make api calls
+        const healthList = [];
+        const fitnessList = [];
+        const selfcareList = [];
         // use setResults to store api results
-        setResults((results) => ({...results, [category]: userSelection[category]=== name ? '' : name,}));
+        
+        // make api call to Edamam API
+        healthList.push('Breakfast Sandwich', 'link to recipe', 'image to recipe')
+
+        // make api call to YouTube API
+        fitnessList.push('10 min ab workout', 'link to workout', 'image for workout')
+
+        // make api call to YouTube API
+        selfcareList.push('Drawing tutorial', 'link to tutorial', 'image for tutorial')
+
+        setResults((results) => ({...results, title: healthList, fitness: fitnessList, selfcare: selfcareList}));
+        
     };
+    
 
     return (
         <div>
