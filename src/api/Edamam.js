fcaa7bb78fc1d0fd29b5936288b3ce1b2c3ae30f
sonashridhar <shridharsona@gmail.com>
2021-04-25 01:50:30 -0700

    integrated edamam with planButton

--- src/api/Edamam.js
+++ src/api/Edamam.js
@@ -4,44 +4,33 @@ require("dotenv").config();
 const KEY = process.env.REACT_APP_EDAMAM_KEY;
 const ID = process.env.REACT_APP_EDAMAM_ID;
 
-const Edamam = ({meal}) => {
-    const handleClick = async () => {
+const Edamam = async ({meal}) => {
 
-        // random food names / ingredients
-        const bfastWords = ["eggs", "parfait", "fruit", "toast", "smoothie", "waffle",
+    const bfastWords = ["eggs", "parfait", "fruit", "toast", "smoothie", "waffle",
                                 "muffin", "yogurt", "baked", "oats", "quick"]
-        const linnerWords = ["salad", "pasta", "casserole", "sandwich", "chicken", "rice",
+    const linnerWords = ["salad", "pasta", "casserole", "sandwich", "chicken", "rice",
                             "soup", "egg", "ham", "saute", "vegetable"]
 
-        const maxEntries = 20;
-        const maxIngr = 5;
+    const maxEntries = 20;
+    const maxIngr = 5;
 
-        if (meal == "Breakfast"){
-            var keyword = bfastWords[Math.floor(bfastWords.length * Math.random())];
-            console.log(keyword);
-        }
-        else {
-            var keyword = linnerWords[Math.floor(linnerWords.length * Math.random())];
-            console.log(keyword);
-        }
-
-        
-        const response = await axios.get(`https://api.edamam.com/search`, {params: 
-            {q: keyword, app_id: ID, app_key: KEY, to: maxEntries, ingr: maxIngr, mealType: meal}
-        })
-
-        const hits = response.data.hits
-        const randIndex = Math.floor(hits.length * Math.random());
-        const hitsMap = hits.map(x => [x.recipe.label, x.recipe.url]);
-        
-        console.log(randIndex);
-        console.log(hitsMap[randIndex]);
+    if (meal === "breakfast"){
+        var keyword = bfastWords[Math.floor(bfastWords.length * Math.random())];
     }
+    else {
+        var keyword = linnerWords[Math.floor(linnerWords.length * Math.random())];
+    }
+        
+    const response = await axios.get(`https://api.edamam.com/search`, {params: 
+        {q: keyword, app_id: ID, app_key: KEY, to: maxEntries, ingr: maxIngr, mealType: meal}
+    })
+
+    const hits = response.data.hits;
+    const randIndex = Math.floor(hits.length * Math.random());
+    const hitsMap = hits.map(x => [x.recipe.label, x.recipe.url, x.recipe.image]);
 
     return (
-        <div>
-            <button type="button" onClick={handleClick}>Get Recipe</button>
-        </div>
+        hitsMap[randIndex]
     )
 }
 
--- src/frontend/components/PlanButton.js
+++ src/frontend/components/PlanButton.js
@@ -1,16 +1,20 @@
 import React, { useState, useEffect } from 'react';
 import './PlanButton.css';
 
+//
+import Edamam from '../../api/Edamam.js'
+
 const PlanButton = ({ userSelection, results, setResults }) => {
 
     const handleClick = async () => {
         const healthList = [];
         const fitnessList = [];
         const selfcareList = [];
-        // use setResults to store api results
-        
-        // make api call to Edamam API
-        healthList.push('Breakfast Sandwich', 'link to recipe', 'image to recipe')
+
+        const healthHits = Edamam(userSelection.health);
+        healthHits.then(function(result) {
+            healthList.push(result[0], result[1], result[2]);
+         })
 
         // make api call to YouTube API
         fitnessList.push('10 min ab workout', 'link to workout', 'image for workout')
